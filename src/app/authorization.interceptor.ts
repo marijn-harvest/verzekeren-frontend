2fefbc68f84b968a78889d5532cc07cc0234b2f4
marijn-harvest <marijn.grimberg@harvest.nl>
2018-07-23 17:51:03 +0200

    401 afvangen

--- src/app/authorization.interceptor.ts
+++ src/app/authorization.interceptor.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY, throwError } from 'rxjs';
+
+import { catchError } from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthorizationInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = request.clone({
       withCredentials: true
     });
 
-    return next.handle(request);
+    return next.handle(request).pipe(catchError( err => {
+      if (err.status === 401) {
+        this.router.navigate(['login']);
+        return EMPTY;
+      } else {
+        return throwError(err);
+      }
+    }));
   }
 }
